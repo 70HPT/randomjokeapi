85e9c97b096ff30d5bafd323721255e922f6d3b8
Jacob Echeverry Espinosa <142534754+70HPT@users.noreply.github.com>
2024-02-04 21:48:01 -0500

    Add files via upload

---
+++ demo/src/main/java/com/randomjoke/demo/DemoApplication.java
@@ -0,0 +1,52 @@
+package com.randomjoke.demo;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@SpringBootApplication
+public class DemoApplication {
+
+
+		public static void main(String[] args) {
+			SpringApplication.run(DemoApplication.class, args);
+			getRandomJoke();
+			System.exit(0);
+		}
+
+		public static void getRandomJoke() {
+			try {
+				String url = "https://official-joke-api.appspot.com/random_joke";
+				RestTemplate restTemplate = new RestTemplate();
+				ObjectMapper mapper = new ObjectMapper();
+
+				String jSonFact = restTemplate.getForObject(url, String.class);
+				JsonNode root = mapper.readTree(jSonFact);
+
+				String joke = root.findValue("type").asText();
+				String setup = root.findValue("setup").asText();
+				String punchline = root.findValue("punchline").asText();
+				String id = root.findValue("id").asText();
+
+				System.out.println("**********Random Joke********");
+				System.out.println("Joke: " + joke);
+				System.out.println("setup: " + setup);
+				System.out.println("punchline" + punchline);
+				System.out.println("id" + id);
+
+			} catch (JsonProcessingException ex) {
+				Logger.getLogger(DemoApplication.class.getName()).log(
+						Level.SEVERE,
+						null, ex);
+				System.out.println("error in getCatFact");
+
+			}
+		}
+}
+
